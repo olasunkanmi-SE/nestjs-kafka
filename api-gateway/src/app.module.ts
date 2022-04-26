1ef061dbdeba85417cef0c54e62cc3a63148c9b0
Olasunkanmi <raymond.oyinlola@gmail.com>
2022-04-26 21:49:07 +0800

    create the docker compose yaml file

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '/nestjs-kafka/.env',
+    }),
     ClientsModule.register([
       {
         name: 'BILLING_SERVICE',
