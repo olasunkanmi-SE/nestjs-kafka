b3c7c96676b3852332863ec0fbb2ea0168b277bd
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2022-04-27 02:01:48 +0800

    Development (#6)
    * Create README.md
    
    * add docker files to applications (#2)
    
    * Feature/infrastructure (#3)
    
    * add docker files to applications
    
    * create the docker compose yaml file
    
    * create the docker compose yaml file
    
    * configure typeorm connection object
    
    * Feature/infrastructure (#5)
    
    * add docker files to applications
    
    * create the docker compose yaml file
    
    * create the docker compose yaml file
    
    * configure typeorm connection object
    
    * add sonar cloud analysis to workflow
    
    * add sonar cloud analysis to workflow
    
    * remove build yaml filr

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -1,9 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dBConfig } from 'ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '/nestjs-kafka/.env' }),
+    TypeOrmModule.forRoot({ ...dBConfig, autoLoadEntities: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
